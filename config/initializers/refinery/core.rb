e906da0397884efd397077d13990b266add5b551
dokoresol <do@koresol.com>
2014-07-10 08:12:17 -0700

    video at home, header social icons alignment

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,12 +20,12 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-   config.site_name = "koresol"
+   config.site_name = "bfskoresol"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
   # Google Analytics are made.
-  # config.google_analytics_page_code = "UA-xxxxxx-x"
+   config.google_analytics_page_code = "UA-52707790-1"
 
   # Enable/disable authenticity token on frontend
   # config.authenticity_token_on_frontend = false
